6cacd32cf0dfc251798901a081d87f2e81590998
anonimnianonist <vanjamba0211@gmail.com>
2026-08-05 21:11:13 +0300

    Update .lua

--- .lua
+++ .lua
@@ -1,3 +1,7 @@
+if not _G.AutofarmRunning then
+    return
+end
+
 queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/vfx-qudsa/asfsadasdasdasdsa/refs/heads/main/.lua'))()")
 
 local LocalPlayer = game:GetService("Players").LocalPlayer
